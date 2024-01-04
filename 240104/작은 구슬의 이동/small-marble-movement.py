44dc36b4e903640c2bcf295a8c1d4980c71e10d1
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-04 13:39:49 +0900

    Title: 작은 구슬의 이동, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240104/작은 구슬의 이동/small-marble-movement.py
+++ 240104/작은 구슬의 이동/small-marble-movement.py
@@ -2,13 +2,14 @@
 n, t = map(int, input().strip().split(' '))
 
 # bead info// row, col, current_direction
-r, c, cur_dir = input().strip().split(' ')
-col = int(r)
-row = int(c)
+# r 행 c 열 i j
+x, y, cur_dir = input().strip().split(' ')
+x = int(x) - 1
+y = int(y) - 1
 
 # L U D R
-dxs = [0, -1, 1, 0] # row 
-dys = [-1, 0, 0, 1]
+dys = [-1, 0, 0, 1]  # 열
+dxs = [0, -1, 1, 0]  # 행
 
 mapper = {
     'L': 0,
@@ -19,8 +20,15 @@ mapper = {
 
 cur_dir = mapper[cur_dir]
 
-def in_range(x, y):
-    return 0 < x and x < n and 0 < y and y < n
+
+def can_not_move_side(x):
+    return x <= 0 or x >= n - 1
+
+
+def can_not_move_upside(y):
+    return y <= 0 or y >= n - 1
+
+
 # gird
 
 # 구슬 처음 위치 grid[row][col]
@@ -29,15 +37,20 @@ def in_range(x, y):
 # !in_range
 def change_dir(dir_num):
     return 3 - dir_num
-# 이동, 시간 1 소요
-def move(dir_num, x, y):
-    x += dxs[dir_num]
-    y += dys[dir_num]
-    return x, y
 
-for _ in range(t):
-    if in_range(row, col):
-        row, col = row + dxs[cur_dir], col + dys[cur_dir]
-        continue
-    cur_dir = change_dir(cur_dir)
-print(col+1, row+1)
+
+cur_t = 0
+while cur_t < t:
+    if cur_dir in [0, 3] and can_not_move_side(y):
+        cur_dir = change_dir(cur_dir)
+        cur_t += 1
+        # print(f"current {cur_t}, x: {x}, y: {y}, d: {cur_dir}")
+    elif cur_dir in [1, 2] and can_not_move_upside(x):
+        cur_dir = change_dir(cur_dir)
+        cur_t += 1
+        # print(f"current {cur_t}, x: {x}, y: {y}, d: {cur_dir}")
+    x, y = x + dxs[cur_dir], y + dys[cur_dir]
+    cur_t += 1
+    # print(f"current {cur_t}, x: {x}, y: {y}, d: {cur_dir}")
+
+print(x + 1, y + 1)

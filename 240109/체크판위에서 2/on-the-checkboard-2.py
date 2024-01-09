fc876f25166ec17f6f058177485f8975d1766e42
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-09 10:50:28 +0900

    Title: 체크판위에서 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240109/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,25 @@
+r, c = map(int, input().split(' '));
+
+
+grid = []
+for _ in range(r):
+    grid.append(input().split(' '))
+
+
+cnt = 0
+for i in range(1, r):
+    for j in range(1, c):
+        for k in range(i+1, r):
+            for l in range(j+1, c):
+                if grid[0][0] =='B' and grid[i][j] == 'W' and grid[k][l] == 'B':
+                    for a in range(k+1, r):
+                        for b in range(l+1, c):
+                            if grid[a][b] == 'W':
+                                cnt += 1
+                elif grid[0][0] =='W' and grid[i][j] =='B' and grid[k][l] == 'W':
+                    for a in range(k+1, r):
+                        for b in range(l+1, c):
+                            if grid[a][b] == 'B':
+                                cnt += 1
+
+print(cnt)

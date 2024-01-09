021cef06f2263ad86832b0bf0dadce5f8c379c9a
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-09 10:57:42 +0900

    Title: 체크판위에서 2, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240109/체크판위에서 2/on-the-checkboard-2.py
+++ 240109/체크판위에서 2/on-the-checkboard-2.py
@@ -9,17 +9,9 @@ for _ in range(r):
 cnt = 0
 for i in range(1, r):
     for j in range(1, c):
-        for k in range(i+1, r):
-            for l in range(j+1, c):
-                if grid[0][0] =='B' and grid[i][j] == 'W' and grid[k][l] == 'B':
-                    for a in range(k+1, r):
-                        for b in range(l+1, c):
-                            if (a == r-1 and b == c-1) and grid[a][b] == 'W':
-                                cnt += 1
-                elif grid[0][0] =='W' and grid[i][j] =='B' and grid[k][l] == 'W':
-                    for a in range(k+1, r):
-                        for b in range(l+1, c):
-                            if (a == r-1 and b == c-1) and grid[a][b] == 'B':
-                                cnt += 1
-
+        for k in range(i+1, r-1):
+            for l in range(j+1, c-1):
+                if grid[i][j] != grid[0][0] and grid[k][l] != grid[i][j] and grid[k][l] != grid[r-1][c-1]:
+                    cnt += 1
+                
 print(cnt)

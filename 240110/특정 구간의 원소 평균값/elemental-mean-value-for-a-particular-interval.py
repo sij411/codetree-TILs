19be274f1f0313fd99a99022df24e4b94ff56455
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-10 10:03:41 +0900

    Title: 특정 구간의 원소 평균값, Time: 176ms, Memory: 25MB, Status: Passed - CodeTree

--- 240110/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240110/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -4,11 +4,12 @@ arr = list(map(int, input().strip().split(" ")))
 
 cnt = 0
 
-for k in range(1, n): # element nums
+for k in range(1, n+1): # element nums
     for i in range(n - k + 1):
         tgt = arr[i:i+k]
         mean = sum(tgt)/k
         if mean in tgt:
+            # print(mean, tgt)
             cnt += 1
 
 print(cnt)

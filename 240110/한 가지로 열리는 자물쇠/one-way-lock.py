32bfa72096b498d100c48114a83c5082fb23fb90
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-10 11:00:56 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 187ms, Memory: 26MB, Status: Passed - CodeTree

--- 240110/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 240110/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -5,11 +5,12 @@ cnt = 0
 for i in range(1, n+1):
     for j in range(1, n+1):
         for k in range(1, n+1):
-            
+            # 조건에 해당하지 않은 조합인지 확인합니다.
             if (abs(i-given[0]) > 2) and (abs(j-given[1]) > 2) and (abs(k-given[2]) > 2) :
                 # print(f"{i} {j} {k}")
                 cnt += 1
-                    
+
+# 전체 경우의 수에서 조건에 해당하지 않는 경우의 수를 제외합니다.                    
 total = n**3   
 result = total - cnt  
 print(total-cnt)

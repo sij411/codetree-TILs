7b019224b969eefe5d0874b9656fce30cbbb443c
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-10 09:29:21 +0900

    Title: G or H 3, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240110/G or H 3/G-or-H-3.py
+++ 240110/G or H 3/G-or-H-3.py
@@ -1,18 +1,25 @@
 n, k = map(int, input().split(" "))
 
 pos = [0 for _ in range(10000)]
+m = 0
 for i in range(n):
     p, alphabet = input().split(" ")
     if alphabet == 'G':
-        pos[int(p)-1] = 1
+        pos[int(p)] = 1
     elif alphabet == 'H':
-        pos[int(p)-1] = 2
+        pos[int(p)] = 2
+    if m < int(p):
+        m = int(p)
+
+# print(m)
 
 
 max_cnt = 0
 cnt = 0
-for j in range(len(pos)-k+1):
-    cnt = sum(pos[j:j+k])
+for j in range(1, m-k+1):
+    # print(pos[j:j+k])
+    cnt = sum(pos[j:j+k+1])
     max_cnt = max(max_cnt, cnt)
+    # print(cnt)
    
 print(max_cnt)

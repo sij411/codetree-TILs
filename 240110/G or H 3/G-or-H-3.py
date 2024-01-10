613a8851b912bc18962449b42d08563268f37d2c
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-10 09:39:06 +0900

    Title: G or H 3, Time: 168ms, Memory: 28MB, Status: Passed - CodeTree

--- 240110/G or H 3/G-or-H-3.py
+++ 240110/G or H 3/G-or-H-3.py
@@ -9,16 +9,12 @@ for i in range(n):
         pos[int(p)] = 1
     elif alphabet == 'H':
         pos[int(p)] = 2
-    if m < int(p):
-        m = int(p)
-    if l > int(p):
-        l = int(p)
+
 # print(m)
 
 max_cnt = 0
 cnt = 0
 for j in range(10000-k+1):
- 
     cnt = sum(pos[j:j+k+1])
     max_cnt = max(max_cnt, cnt)
    

2ab4aa3163b65a10f61b88fe4173c6a7ac1bdf6e
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-10 14:00:24 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240110/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ 240110/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -6,19 +6,19 @@ cord = []
 for _ in range(n):
     arr = list(map(int, input().split(' ')))
     cord.append(arr)
-
+min_square = sys.maxsize
 for i in range(n): # 제외시킬 좌표의 인덱스
-    min_square = sys.maxsize
-    for j in range(n):
-        max_x, max_y, min_x, min_y = -sys.maxsize, -sys.maxsize, sys.maxsize, sys.maxsize
-        for idx, pos in enumerate(cord):
-            if idx == i:
-                continue
+    
+
+    max_x, max_y, min_x, min_y = -sys.maxsize, -sys.maxsize, sys.maxsize, sys.maxsize
+    for idx, pos in enumerate(cord):
+        if idx == i:
+            continue
             
-            max_x = max(max_x, pos[0])
-            max_y = max(max_y, pos[1])
-            min_x = min(min_x, pos[0])
-            min_y = min(min_y, pos[1])
+        max_x = max(max_x, pos[0])
+        max_y = max(max_y, pos[1])
+        min_x = min(min_x, pos[0])
+        min_y = min(min_y, pos[1])
     square = abs(max_x - min_x) * abs(max_y - min_y)
     min_square = min(min_square, square)
 

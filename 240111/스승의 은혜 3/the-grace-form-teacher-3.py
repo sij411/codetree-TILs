1ed114d01589ace145b9faa342bc155c32414b45
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-11 15:08:30 +0900

    Title: 스승의 은혜 3, Time: 374ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240111/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240111/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -8,23 +8,28 @@ for _ in range(n):
 
 # print(students)
 
-def calculate(discount_idx):
+def calculate(discount_idx, students):
     total = 0
     cnt = 0
     for idx, order in enumerate(students):
-        if total >= budget:
-            return total, cnt
         if idx == discount_idx:
             price, shipping = order[0]/2, order[1]
         else:
             price, shipping = order[0], order[1]
         cnt += 1
         total += price + shipping
+        if total >= budget:
+            return total, cnt
     return total, cnt
+
+def c_sort(item):
+    return item[0]/2 + item[1]
 
 max_cnt = 0
 for i in range(n):
-    total, cnt = calculate(i)
+    arr_sorted = sorted(students, key=c_sort)
+    total, cnt = calculate(i, arr_sorted)
+    # print(i, total, cnt)
     cnt = max(max_cnt, cnt)
 
 print(cnt)

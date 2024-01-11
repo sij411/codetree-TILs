916c8fb2a127bf66173c64bf73c88f5f63403ce8
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-11 16:09:41 +0900

    Title: 스승의 은혜 3, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240111/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240111/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,4 +1,3 @@
-import sys
 # input: students, budget
 
 n, budget = map(int, input().strip().split(" "))
@@ -17,21 +16,21 @@ def calculate(discount_idx, students):
             price, shipping = order[0]//2, order[1]
         else:
             price, shipping = order[0], order[1]
-        
-        if (total + price + shipping) <= budget:
-            cnt += 1
-            total += price + shipping
-            continue
-        return total, cnt
+        if (total + price + shipping) >= budget:
+            return total, cnt
+        cnt += 1
+        total += price + shipping
+    return total, cnt
 
 def c_sort(item):
     return item[0] + item[1]
 
-max_cnt = -sys.maxsize
+max_cnt = 0
 arr_sorted = sorted(students, key=c_sort)
 for i in range(n):
     total, cnt = calculate(i, arr_sorted)
     # print(i, total, cnt)
-    max_cnt = max(max_cnt, cnt)
+    if total <= budget:
+        cnt = max(max_cnt, cnt)
 
-print(max_cnt)
+print(cnt)

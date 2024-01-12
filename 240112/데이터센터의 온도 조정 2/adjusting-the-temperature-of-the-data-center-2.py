80c8292ae6e29bcd814791db10c3d8bb16df8625
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-12 09:57:47 +0900

    Title: 데이터센터의 온도 조정 2, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240112/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
+++ 240112/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -1,25 +1,23 @@
-n, c, g, f = map(int, input().strip().split(" "))
+n, c, g, h = map(int, input().strip().split(" "))
 
 def get_workload(cur_t, ta, tb):
     if cur_t < ta:
         return c
-    elif ta <= cur_t <= tb:
+    elif cur_t <= tb:
         return g
-    elif cur_t > tb:
-        return f
     else:
-        return 0
+        return h
 
 max_total = 0
 arr = []
+
 for _ in range(n):
     arr.append(list(map(int, input().split(" "))))
 
 for t in range(1001):
     cur_workload = 0
     for i in range(n):
-        ta, tb = arr[i][0], arr[i][1]
-        cur_workload += get_workload(t, ta, tb)
+        cur_workload += get_workload(t, arr[i][0], arr[i][1])
     max_total = max(max_total, cur_workload)
 
 print(max_total)

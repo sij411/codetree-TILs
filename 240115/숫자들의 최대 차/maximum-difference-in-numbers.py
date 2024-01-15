4b77f47b880ef521253ec3f356cb809b0aaf82fc
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-15 14:00:46 +0900

    Title: 숫자들의 최대 차, Time: 5102ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240115/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240115/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -3,6 +3,7 @@ elements = []
 for _ in range(n):
     elements.append(int(input()))
 
+elements.sort()
 max_cnt = 0
 
 for k in range(K+1):
@@ -11,9 +12,7 @@ for k in range(K+1):
             cnt = 0
             if abs(elements[i] - elements[j]) == k:
                 # print(elements[i], elements[j])
-                for e in elements:
-                    if (elements[i] <= e <= elements[j]) or (elements[j] <= e <= elements[i]):
-                        cnt += 1
+                cnt += len(elements[i:j+1])
                 max_cnt = max(cnt, max_cnt)
                 # print(k,i,j, max_cnt)
                 

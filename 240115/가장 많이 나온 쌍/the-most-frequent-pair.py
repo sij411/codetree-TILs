016f84088a59d3b342eb35f449e0bc59f75c2045
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-15 13:28:54 +0900

    Title: 가장 많이 나온 쌍, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240115/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 240115/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -12,16 +12,8 @@ m_cnt = 0
 for x, y in pairs:
     cnt = 0
     for a, b in pairs:
-        if x < y:
-            if a < b and compare(x, y, a, b):
-                cnt += 1
-            elif a > b and compare(x, y, b, a):
-                cnt += 1
-        else:
-            if a < b and compare(x, y, b, a):
-                cnt += 1
-            elif a > b and compare(x, y, a, b):
-                cnt += 1
+        if (x == a and y == b) or (x == b and y == a):
+            cnt += 1
     m_cnt = max(cnt, m_cnt)
 
 

f779668d2de9ec5a90da5177d567161686714c20
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-16 15:30:05 +0900

    (update) update the latest version of my algorithm

--- 240115/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240115/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -3,23 +3,28 @@ elements = []
 for _ in range(n):
     elements.append(int(input()))
 
-elements.sort()
+elements = sorted(elements)
 max_cnt = 0
 max_elem = max(elements)
 min_elem = min(elements)
 diff = max_elem - min_elem
+
 for k in range(K+1):
     if k > diff:
         continue
-    for i in range(n):
-        for j in range(i+1, n):
-            cnt = 0
-            if elements[j] - elements[i] > k:
-                continue
-                # print(elements[i], elements[j])
-            cnt += len(elements[i:j+1])
+    i, j = 0, 1
+    while j < n :
+        if elements[j] - elements[i] == k and i != j:
+            cnt = j - i + 1 # idx 차이 == 개수 이미 정렬했으므로 해당 인덱스 범위 내에 있는 숫자들은 구간 안에 있음. 
             max_cnt = max(cnt, max_cnt)
-                # print(k,i,j, max_cnt)
+            # print(f"k: {k}, i: {i}, j: {j}, cnt: {cnt}") 
+            j += 1
+        elif elements[j] - elements[i] > k:
+            i += 1
+        else:
+            j += 1
+    
+    
                 
 
 print(max_cnt)

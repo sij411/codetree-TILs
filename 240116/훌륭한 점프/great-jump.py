f8ea3357f1595d43d346741cc148614b06a382e9
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-16 11:36:10 +0900

    Title: 훌륭한 점프, Time: 236ms, Memory: 25MB, Status: Passed - CodeTree

--- 240116/훌륭한 점프/great-jump.py
+++ 240116/훌륭한 점프/great-jump.py
@@ -6,7 +6,7 @@ def is_possible(maximum):
     available = [i for i, elem in enumerate(arr) if elem <= maximum]
     for i in range(1, len(available)):
         dist = available[i] - available[i - 1]
-        if dist > 2:
+        if dist > k:
             return False
     return True
 
@@ -16,6 +16,6 @@ min_in_max = max(arr) + 1
 for a in range(max(arr[0], arr[-1]), max(arr) + 1):
     if is_possible(a):
         min_in_max = min(min_in_max, a)
-
+   
 
 print(min_in_max)

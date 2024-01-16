5d7ddf95f5f016c067d1eb0eacc67e932558d239
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-16 10:24:59 +0900

    Title: 숫자들의 최대 차, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240116/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240116/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -1,28 +1,23 @@
-n, K = map(int, input().split(" "))
+n, k = map(int, input().split(" "))
 elements = []
 for _ in range(n):
     elements.append(int(input()))
 
 elements = sorted(elements)
 max_cnt = 0
-max_elem = max(elements)
-min_elem = min(elements)
-diff = max_elem - min_elem
 
-for k in range(K+1):
-    if k > diff:
-        continue
-    i, j = 0, 1
-    while j < n :
-        if elements[j] - elements[i] == k and i != j:
-            cnt = j - i + 1 # idx 차이 == 개수 이미 정렬했으므로 해당 인덱스 범위 내에 있는 숫자들은 구간 안에 있음. 
-            max_cnt = max(cnt, max_cnt)
+
+i, j = 0, 1
+while j < n :
+    if elements[j] - elements[i] == k and i != j:
+        cnt = j - i + 1 # idx 차이 == 개수 이미 정렬했으므로 해당 인덱스 범위 내에 있는 숫자들은 구간 안에 있음. 
+        max_cnt = max(cnt, max_cnt)
             # print(f"k: {k}, i: {i}, j: {j}, cnt: {cnt}") 
-            j += 1
-        elif elements[j] - elements[i] > k:
-            i += 1
-        else:
-            j += 1
+        j += 1
+    elif elements[j] - elements[i] > k:
+        i += 1
+    else:
+        j += 1
     
     
                 

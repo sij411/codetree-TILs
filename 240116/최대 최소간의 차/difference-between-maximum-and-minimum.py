7379352ab8ed13cd23d2cf2180237f3db7d19ebd
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-16 13:48:18 +0900

    Title: 최대 최소간의 차, Time: 461ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240116/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 240116/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -4,35 +4,26 @@ n, k = map(int, input().split(" "))
 
 arr = list(map(int, input().strip().split(" ")))
 
-# find max min pairs
-def find_max_min_pairs(k):
-    arr = []
-    for a in range(10000):
-        for b in range(10000):
-            if a - b == k:
-                arr.append((a, b))
-    return arr
 
-def calculate(k): 
-    pairs = find_max_min_pairs(k)
+def calculate(k,min_num, max_num): 
     min_cost = sys.maxsize
-    for max_num, min_num in pairs:
-        cost = 0
-        for elem in arr:
-            if elem == max_num or elem == min_num:
-                continue
-            if elem < min_num:
-                cost += abs(min_num - elem)
-            elif elem > max_num:
-                cost += abs(max_num - elem)
-        min_cost = min(cost, min_cost)
+    cost = 0
+    for elem in arr:
+        if elem == max_num or elem == min_num:
+            continue
+        if elem < min_num:
+            cost += abs(min_num - elem)
+        elif elem > max_num:
+            cost += abs(max_num - elem)
+    min_cost = min(cost, min_cost)
     return min_cost # 해당 k 한정
         
 
 min_cost = sys.maxsize    
-
+cost_ = 0
 for i in range(k+1):
-    cost = calculate(i)
-    min_cost = min(cost, min_cost)
+    for elem in arr:
+        cost_ = calculate(i, elem, elem+i)
+        min_cost = min(cost_, min_cost)
 
 print(min_cost)

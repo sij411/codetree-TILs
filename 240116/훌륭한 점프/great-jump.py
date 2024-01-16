5d7ef4837cb5b65033b2c0c2b7ba9679f3d72f15
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-16 11:28:21 +0900

    Title: 훌륭한 점프, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240116/훌륭한 점프/great-jump.py
+++ 240116/훌륭한 점프/great-jump.py
@@ -3,7 +3,7 @@ n, k = map(int, input().split(" "))
 arr = list(map(int, input().split(" ")))
 
 def is_possible(maximum):
-    available = [i for i, elem in enumerate(arr) if elem >= maximum]
+    available = [i for i, elem in enumerate(arr) if elem <= maximum]
     for i in range(1, len(available)):
         dist = available[i] - available[i - 1]
         if dist > 2:
@@ -11,11 +11,11 @@ def is_possible(maximum):
     return True
 
 
-min_in_max = 0
+min_in_max = max(arr) + 1
 
-for a in range(n - 1, -1, -1):
+for a in range(max(arr[0], arr[-1]), max(arr) + 1):
     if is_possible(a):
-        min_in_max = max(min_in_max, a)
+        min_in_max = min(min_in_max, a)
 
 
 print(min_in_max)

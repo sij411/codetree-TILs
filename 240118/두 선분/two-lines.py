d0ca807828a786aec3f882e9bf5aa86d501944dc
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-18 13:37:56 +0900

    Title: 두 선분, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240118/두 선분/two-lines.py
+++ 240118/두 선분/two-lines.py
@@ -2,10 +2,10 @@ arr = list(map(int, input().strip().split(" ")))
 
 
 for i in range(len(arr)):
-    if i < 2 and (arr[i] >= arr[2] or arr[i] <= arr[3]):
+    if i <= 1 and (arr[i] >= arr[2] and arr[i] <= arr[3]):
         print("intersecting")
         break
-    elif i > 1 and (arr[i] >= arr[0] or arr[i] <= arr[1]):
+    elif i > 1 and (arr[i] >= arr[0] and arr[i] <= arr[1]):
         print("intersecting")
         break
     else:

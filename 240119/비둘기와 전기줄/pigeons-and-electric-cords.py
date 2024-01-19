55ec4d29a0ab24e1d03d74190cd4aae46c46ae7b
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-19 17:18:59 +0900

    Title: 비둘기와 전기줄, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240119/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+pigeons = [[] for _ in range(n+1)]
+
+for _ in range(n):
+    idx, d = map(int, input().strip().split(" "))
+    pigeons[idx].append(d)
+
+cnt = 0
+for p, move in enumerate(pigeons):
+    if len(move) < 2:
+        continue
+    for m in range(1, len(move)):
+        if move[m] != move[m-1]:
+            cnt += 1
+
+print(cnt)

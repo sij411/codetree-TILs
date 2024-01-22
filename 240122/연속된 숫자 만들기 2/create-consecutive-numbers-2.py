f297ca0d3ffa106ff53217baf21fe37455fbd653
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-22 16:40:34 +0900

    Title: 연속된 숫자 만들기 2, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240122/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,17 +1,14 @@
 a, b, c = map(int, input().strip().split(" "))
 
-all_seq = False
-
 
 def check_seq(a, b, c):
     if (a + 1) == b and (b + 1) == c:
-        all_seq = True
-    return all_seq
-
-all_seq = check_seq(a, b, c)
+        return True
+    return False
 
 
-if all_seq:
+
+if check_seq(a, b, c):
     cnt = 0
 else:
     if (b - a) == 2 or (c - b) == 2:

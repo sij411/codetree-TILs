0412480bfa3e8c2bc511c1989d2f18ed7b71a54f
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-23 12:39:26 +0900

    Title: 작은 구슬의 이동, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240123/작은 구슬의 이동/small-marble-movement.py
+++ 240123/작은 구슬의 이동/small-marble-movement.py
@@ -9,13 +9,13 @@ y = int(y) - 1
 
 
 # L U D R
-dys = [-1, 0, 0, 1]  # 열
-dxs = [0, -1, 1, 0]  # 행
+dys = [-1, 0, 1, 0]  # 열
+dxs = [0, -1, 0, 1]  # 행
 
 mapper = {
     'L': 0,
     'U': 1,
-    'D': 2,
+    'R': 2,
     'R': 3
 }
 
@@ -25,8 +25,7 @@ cur_dir = mapper[cur_dir]
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
-def change_dir(dir_num):
-    return 3 - dir_num
+
 
 
 for _ in range(t):
@@ -34,7 +33,7 @@ for _ in range(t):
     if in_range(nx, ny):
         x, y = nx, ny
     else:
-        cur_dir = change_dir(cur_dir)
+        cur_dir = (cur_dir + 2) % 4
 
 
 print(x + 1, y + 1)

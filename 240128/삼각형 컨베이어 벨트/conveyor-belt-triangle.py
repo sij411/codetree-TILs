d9ab25769a5fa5cd0d6ef50aa9d06eeae7eeba00
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-28 16:27:17 +0900

    Title: 삼각형 컨베이어 벨트, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240128/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,20 @@
+n, t = map(int, input().split(" "))
+arr = []
+for _ in range(n):
+    arr.extend(list(map(int, input().strip().split(" "))))
+
+
+length = len(arr)
+
+for _ in range(t):
+    tmp = arr[0]
+    for i in range(length - 1, 0, -1):
+        move = (i + 1)%(length)
+        arr[move] = arr[i] 
+    arr[1] = tmp
+
+
+for i in range(length):
+    if i > 0 and i % 3 == 0:
+        print()
+    print(arr[i], end=" ")

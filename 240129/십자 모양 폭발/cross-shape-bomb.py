0da833ddc9e67d8120928ee5232959341cd53769
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-29 01:37:35 +0900

    Title: 십자 모양 폭발, Time: 157ms, Memory: 26MB, Status: Passed - CodeTree

--- 240129/십자 모양 폭발/cross-shape-bomb.py
+++ 240129/십자 모양 폭발/cross-shape-bomb.py
@@ -12,21 +12,28 @@ r, c = map(int, input().split(" "))
 r, c = r - 1, c - 1
 
 for k in range(grid[r][c]):
-    grid[r + k][c] = 0
-    grid[r - k][c] = 0
-    grid[r][c + k] = 0
-    grid[r][c - k] = 0
+    if r + k < n:
+        grid[r + k][c] = 0
+    if r - k >= 0:
+        grid[r - k][c] = 0
+    if c + k < n:
+        grid[r][c + k] = 0
+    if c - k >= 0:
+        grid[r][c - k] = 0
 
-# 폭탄 터진 후 확인
-print(grid)
 
 temp = [[0] * n for _ in range(n)]
 
-print(temp_row)
+
 for col in range(n):
     temp_row = n - 1
     for row in range(n - 1, -1, -1):
-        temp[3][col] = grid[row][col] # gravity -> 맨 밑부터 쌓여야 함
-        temp_row -= 1
-       
-print(temp)
+        if grid[row][col] != 0:
+            temp[temp_row][col] = grid[row][col] # gravity -> 맨 밑부터 쌓여야 함
+            temp_row -= 1
+        
+for i in range(n):
+    for j in range(n):
+        grid[i][j] = temp[i][j]
+        print(grid[i][j], end=" ")
+    print()

b7022d3ae0963eaf9be0e3cd951598f9ab7773f1
Jiwon Kwon <kjiwon411@gmail.com>
2024-01-29 11:43:25 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 390ms, Memory: 30MB, Status: Passed - CodeTree

--- 240129/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 240129/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -9,27 +9,21 @@ def print_permutation():
         print(num, end = " ")
     print()
 
-def condition(selected_nums, i):
-    return selected_nums[-2] != selected_nums[-1] or selected_nums[-1] != i
 
-def find_permutations(cnt):
+def find_duplicated_permutations(cnt):
     # n개를 모두 뽑은 경우 답을 출력해줍니다.
     if cnt == n:
         print_permutation()
         return
-    # 조건: 3번 이상 연속 숫자가 나오면 안됨
-    # -> cnt >= 3 -1, -2 모두 다른 숫자여야 함.
-
-    # 1부터 k까지의 각 숫자가 뽑혔을 때의 경우를 탐색합니다.
+    
     for i in range(1, k + 1):
-    # 조건: 3번 이상 연속 숫자가 나오면 안됨
-    # -> cnt >= 3 -1, -2 모두 다른 숫자여야 함.
-
-    # 2개면 그냥 넣어
-        if cnt < 2 or condition(selected_nums, i):
+        if cnt >= 2 and i == selected_nums[-1] and \
+                        i == selected_nums[-2]:
+            continue
+        else:
             selected_nums.append(i)
-            find_permutations(cnt + 1)
+            find_duplicated_permutations(cnt + 1)
             selected_nums.pop()
 
 
-find_permutations(0)
+find_duplicated_permutations(0)

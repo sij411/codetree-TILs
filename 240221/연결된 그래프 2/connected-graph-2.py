ddbd12df5f7365cbdb10c5a57f2147893a6468b2
Jiwon Kwon <kjiwon411@gmail.com>
2024-02-21 16:23:09 +0900

    Title: 연결된 그래프 2, Time: 165ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240221/연결된 그래프 2/connected-graph-2.py
+++ 240221/연결된 그래프 2/connected-graph-2.py
@@ -1,32 +1,30 @@
 # 입력
 n, m = map(int, input().split(" "))
 graph = [[] for _ in range(n+1)]
-visited = [False for _ in range(n+1)]
+
 count = [0 for _ in range(n+1)]
 for _ in range(m):
     v, e = map(int, input().split(" "))
     graph[v].append(e)
-    graph[e].append(v)
+    
 # 가장 많은 정점으로 갈 수 있다 -> 연결된 정점이 젤 많다?
 
 def dfs(vertex):
-    global cnt
+    
     for curr_v in graph[vertex]:
-        if not visited[curr_v]:
-            visited[curr_v] = True
-            cnt += 1
-            dfs(curr_v)
+     
+        count[curr_v] += 1
+     
 
 
-max_cnt = 0
-ans = 1
-for v in range(n+1):
-    visited[v] = True
-    cnt = 0
+for v in range(1, n+1):
     dfs(v)
-    if cnt > max_cnt:
-        max_cnt = cnt
-        ans = v
 
-for v in graph[v]:
-    print(v, end=" ")
+
+
+ans = count.index(max(count))
+for i in range(1, n+1):
+    for e in graph[i]:
+        # print(i, e, ans)
+        if e == ans:
+            print(i, end=" ")

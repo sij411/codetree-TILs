1bedce04c2baf0c45add3691fe98ea34f78f5962
Jiwon Kwon <kjiwon411@gmail.com>
2024-03-02 20:57:33 +0900

    Title: 반복 출력하기, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240302/반복 출력하기/repeated-output.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int rep = sc.nextInt();
+
+        for (int i = 0 ; i < rep; i++) {
+            System.out.println("12345^&*()_");
+        }
+    }
+}
